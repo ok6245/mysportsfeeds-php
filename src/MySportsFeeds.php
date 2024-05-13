e39154c396290d944a67f0bdaf47237ad77fc0c8
Keith at kite <koswald@sbcglobal.net>
2024-05-13 12:40:46 -0700

    Update wrapper version to 2.1.1, require PHP >= 7.4.0 - MySportsFeeds::buildVersion = '2.1.1' to show support for API v2.1 - remove unneeded "use" statements from MySportsFeeds - update composer.json for required PHP version from 5.6 to 7.4.0 - update README.md with proposed new Release number 2.1.1

--- src/MySportsFeeds.php
+++ src/MySportsFeeds.php
@@ -2,75 +2,68 @@
 
 namespace MySportsFeeds;
 
-use MySportsFeeds\API_v1_0;
-use MySportsFeeds\API_v1_1;
-use MySportsFeeds\API_v1_2;
-use MySportsFeeds\API_v2_0;
-
 class MySportsFeeds {
 
-  public $buildVersion = "2.1.0"; // PHP Wrapper version
+    public $buildVersion = "2.1.1"; // PHP Wrapper version
+    
+    private $api_version;
+    private $verbose;
+    private $storeType;
+    private $storeLocation;
+    private $apiInstance;
 
-  private $api_version;
-  private $verbose;
-  private $storeType;
-  private $storeLocation;
-
-  private $apiInstance;
-
-  public function __construct($api_version = "1.2", $verbose = false, $storeType = "file",
+    public function __construct($api_version = "1.2", $verbose = false, $storeType = "file",
                               $storeLocation = "results/") {
 
-    $this->__verifyStore($storeType, $storeLocation);
+        $this->__verifyStore($storeType, $storeLocation);
 
-    $this->api_version = $api_version;
-    $this->verbose = $verbose;
-    $this->storeType = $storeType;
-    $this->storeLocation = $storeLocation;
-    $this->apiInstance = ApiFactory::create($this->api_version, $this->verbose, $this->storeType, $this->storeLocation);
-  }
-
-  # Verify the type and location of the stored data
-  private function __verifyStore($storeType, $storeLocation) {
-    if ($storeType !== null && $storeType != "file") {
-      throw new \ErrorException("Unrecognized storage type specified.  Supported values are: {null,'file'}");
+        $this->api_version = $api_version;
+        $this->verbose = $verbose;
+        $this->storeType = $storeType;
+        $this->storeLocation = $storeLocation;
+        $this->apiInstance = ApiFactory::create($this->api_version, $this->verbose, $this->storeType, $this->storeLocation);
     }
 
-    if ($storeType == "file") {
-      if ($storeLocation === null) {
-        throw new \ErrorException("Must specify a location for stored data.");
-      }
-    }
-  }
+    # Verify the type and location of the stored data
+    private function __verifyStore($storeType, $storeLocation) {
+        if ($storeType !== null && $storeType != "file") {
+            throw new \ErrorException("Unrecognized storage type specified.  Supported values are: {null,'file'}");
+        }
 
-  # Return an array of feed settings in this API version, indexed by feed name.
-  public function getFeedsList() {
-      return $this->apiInstance->getFeedsList();
-  }
-
-  # Get the HTTP response code from the most recent call to getData()
-  public function getHTTPCode() {
-      return $this->apiInstance->getHTTPCode();
-  }
-
-  # Get the URL used for the most recent call to getData()
-  public function getURL() {
-      return $this->apiInstance->getURL();
-  }
-
-  # Authenticate against the API (for v1.x, v2.x)
-  public function authenticate($apikey, $password) {
-    if (!$this->apiInstance->supportsBasicAuth()) {
-      throw new \ErrorException("BASIC authentication not supported for API version " + $this->api_version);
+        if ($storeType == "file") {
+            if ($storeLocation === null) {
+                throw new \ErrorException("Must specify a location for stored data.");
+            }
+        }
     }
 
-    $this->apiInstance->setAuthCredentials($apikey, $password);
-  }
+    # Return an array of feed settings in this API version, indexed by feed name.
+    public function getFeedsList() {
+        return $this->apiInstance->getFeedsList();
+    }
 
-  # Request data (and store it if applicable)
-  public function getData($league, $season, $feed, $format, ...$kvParams) {
+    # Get the HTTP response code from the most recent call to getData()
+    public function getHTTPCode() {
+        return $this->apiInstance->getHTTPCode();
+    }
 
-    return $this->apiInstance->getData($league, $season, $feed, $format, ...$kvParams);
-  }
+    # Get the URL used for the most recent call to getData()
+    public function getURL() {
+        return $this->apiInstance->getURL();
+    }
 
+    # Authenticate against the API (for v1.x, v2.x)
+    public function authenticate($apikey, $password) {
+        if (!$this->apiInstance->supportsBasicAuth()) {
+            throw new \ErrorException("BASIC authentication not supported for API version " + $this->api_version);
+        }
+
+        $this->apiInstance->setAuthCredentials($apikey, $password);
+    }
+
+    # Request data (and store it if applicable)
+    public function getData($league, $season, $feed, $format, ...$kvParams) {
+
+        return $this->apiInstance->getData($league, $season, $feed, $format, ...$kvParams);
+    }
 }
